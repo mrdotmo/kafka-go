7fccfe8fd90925ebbf4dfe68c0b049c443adc549
mrdotmo <mrdotmo>
2024-10-23 15:47:38 -0600

    Renamed Header to RequestHeader

--- header.go
+++ header.go
@@ -2,15 +2,15 @@ package kafka
 
 import "errors"
 
-type Header struct {
+type RequestHeader struct {
 	correlationId [4]byte
 }
 
 // parseHeader extracts the correlation ID from the header.
 //
 //lint:ignore U1000 Disable unused lint check until later.
-func parseHeader(header []byte) (Header, error) {
-	var rv Header
+func parseHeader(header []byte) (RequestHeader, error) {
+	var rv RequestHeader
 	if len(header) < 8 {
 		return rv, errors.New("header must have at least 8 bytes")
 	}
